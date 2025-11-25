61d739be1073e65ef374e6ba2ed3b7f23068ab4a
aldrichjobog <aldrichjobog18@gmail.com>
2025-11-25 13:21:23 +0800

    Update the gallery and added images

--- data.js
+++ data.js
@@ -2,49 +2,49 @@ var galleryItems = [
     // ======== NAVY MARINES (8) ========
     {
         category: "marine",
-        image: "images/sengoku.jpg",
+        image: "images/sengoku.png",
         title: "Sengoku",
         description: "Position: Fleet Admiral"
     },
     {
         category: "marine",
-        image: "images/garp.jpg",
+        image: "images/garp.png",
         title: "Monkey D. Garp",
         description: "Position: Vice Admiral"
     },
     {
         category: "marine",
-        image: "images/akainu.jpg",
+        image: "images/akainu.png",
         title: "Akainu (Sakazuki)",
         description: "Position: Fleet Admiral"
     },
     {
         category: "marine",
-        image: "images/aokiji.jpg",
+        image: "images/aokiji.png",
         title: "Aokiji (Kuzan)",
         description: "Position: Admiral"
     },
     {
         category: "marine",
-        image: "images/kizaru.jpg",
+        image: "images/kizaru.png",
         title: "Kizaru (Borsalino)",
         description: "Position: Admiral"
     },
     {
         category: "marine",
-        image: "images/fujitora.jpg",
+        image: "images/fujitora.png",
         title: "Fujitora (Issho)",
         description: "Position: Admiral"
     },
     {
         category: "marine",
-        image: "images/smoker.jpg",
+        image: "images/smoker.png",
         title: "Smoker",
         description: "Position: Vice Admiral"
     },
     {
         category: "marine",
-        image: "images/tashigi.jpg",
+        image: "images/tashigi.png",
         title: "Tashigi",
         description: "Position: Captain"
     },
@@ -52,50 +52,52 @@ var galleryItems = [
     // ======== PIRATES (8) ========
     {
         category: "pirate",
-        image: "images/luffy.jpg",
+        image: "images/luffy.png",
         title: "Monkey D. Luffy",
         description: "Bounty: 3,000,000,000 Berries"
     },
     {
         category: "pirate",
-        image: "images/zoro.jpg",
+        image: "images/zoro.png",
         title: "Roronoa Zoro",
         description: "Bounty: 1,111,000,000 Berries"
     },
     {
         category: "pirate",
-        image: "images/law.jpg",
+        image: "images/law.png",
         title: "Trafalgar D. Water Law",
         description: "Bounty: 3,000,000,000 Berries"
     },
     {
         category: "pirate",
-        image: "images/kid.jpg",
+        image: "images/kid.png",
         title: "Eustass Kid",
         description: "Bounty: 3,000,000,000 Berries"
     },
     {
         category: "pirate",
-        image: "images/shanks.jpg",
+        image: "images/shanks.png",
         title: "Shanks",
         description: "Bounty: 4,048,900,000 Berries"
     },
     {
         category: "pirate",
-        image: "images/blackbeard.jpg",
+        image: "images/blackbeard.png",
         title: "Marshall D. Teach (Blackbeard)",
         description: "Bounty: 3,996,000,000 Berries"
     },
     {
         category: "pirate",
-        image: "images/ace.jpg",
+        image: "images/ace.png",
         title: "Portgas D. Ace",
         description: "Bounty: 550,000,000 Berries"
     },
     {
         category: "pirate",
-        image: "images/boa.jpg",
+        image: "images/boa.png",
         title: "Boa Hancock",
         description: "Bounty: 1,659,000,000 Berries"
     }
 ];
+
+    
