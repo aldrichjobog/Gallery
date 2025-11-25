5ec8f026dd664ba2a26e2e003d25807f13bf98d2
aldrichjobog <aldrichjobog18@gmail.com>
2025-11-26 00:57:12 +0800

    update gallery

--- data.js
+++ data.js
@@ -1,21 +1,22 @@
 var galleryItems = [
-    // NAVY MARINES
+    // MARINES
     { category: "marine", image: "images/sengoku.png", title: "Sengoku", description: "Position: Fleet Admiral | Former leader, known for strategic genius." },
     { category: "marine", image: "images/garp.png", title: "Monkey D. Garp", description: "Position: Vice Admiral | Legendary hero, famous for capturing pirates." },
     { category: "marine", image: "images/akainu.png", title: "Akainu (Sakazuki)", description: "Position: Fleet Admiral | Strict adherent to absolute justice." },
     { category: "marine", image: "images/aokiji.png", title: "Aokiji (Kuzan)", description: "Position: Admiral | Calm and powerful ice user." },
-    { category: "marine", image: "images/kizaru.png", title: "Kizaru (Borsalino)", description: "Position: Admiral | Moves at light speed and delivers swift justice." },
+    { category: "marine", image: "images/kizaru.png", title: "Kizaru (Borsalino)", description: "Position: Admiral | Moves at light speed." },
     { category: "marine", image: "images/fujitora.png", title: "Fujitora (Issho)", description: "Position: Admiral | Blind but incredibly strong, controls gravity." },
-    { category: "marine", image: "images/smoker.png", title: "Smoker", description: "Position: Vice Admiral | Persistent marine chasing down pirates." },
-    { category: "marine", image: "images/tashigi.png", title: "Tashigi", description: "Position: Captain | Skilled swordswoman and dedicated to justice." },
+    { category: "marine", image: "images/smoker.png", title: "Smoker", description: "Position: Vice Admiral | Persistent marine chasing pirates." },
+    { category: "marine", image: "images/tashigi.png", title: "Tashigi", description: "Position: Captain | Skilled swordswoman." },
 
     // PIRATES
-    { category: "pirate", image: "images/luffy.png", title: "Monkey D. Luffy", description: "Bounty: 3,000,000,000 Berries | Captain of Straw Hat Pirates, wants to be Pirate King." },
-    { category: "pirate", image: "images/zoro.png", title: "Roronoa Zoro", description: "Bounty: 1,111,000,000 Berries | Luffy’s swordsman aiming to be world's greatest swordsman." },
-    { category: "pirate", image: "images/law.png", title: "Trafalgar D. Water Law", description: "Bounty: 3,000,000,000 Berries | Captain of Heart Pirates, master of Op-Op powers." },
-    { category: "pirate", image: "images/kid.png", title: "Eustass Kid", description: "Bounty: 3,000,000,000 Berries | Ruthless captain with magnetic powers." },
-    { category: "pirate", image: "images/shanks.png", title: "Shanks", description: "Bounty: 4,048,900,000 Berries | Red-Haired Yonko, admired by all for his strength and haki." },
-    { category: "pirate", image: "images/blackbeard.png", title: "Marshall D. Teach (Blackbeard)", description: "Bounty: 3,996,000,000 Berries | Dangerous Yonko with two Devil Fruit powers." },
-    { category: "pirate", image: "images/ace.png", title: "Portgas D. Ace", description: "Bounty: 550,000,000 Berries | Luffy’s brother, master of fire powers." },
-    { category: "pirate", image: "images/boa.png", title: "Boa Hancock", description: "Bounty: 1,659,000,000 Berries | Pirate Empress, capable of petrifying enemies." }
+    { category: "pirate", image: "images/luffy.png", title: "Monkey D. Luffy", description: "Bounty: 3,000,000,000 | Captain of Straw Hat Pirates." },
+    { category: "pirate", image: "images/zoro.png", title: "Roronoa Zoro", description: "Bounty: 1,111,000,000 | World's greatest swordsman in training." },
+    { category: "pirate", image: "images/law.png", title: "Trafalgar D. Water Law", description: "Bounty: 3,000,000,000 | Captain of Heart Pirates." },
+    { category: "pirate", image: "images/kid.png", title: "Eustass Kid", description: "Bounty: 3,000,000,000 | Fearless magnetic user." },
+    { category: "pirate", image: "images/shanks.png", title: "Shanks", description: "Bounty: 4,048,900,000 | Yonko with unmatched haki." },
+    { category: "pirate", image: "images/blackbeard.png", title: "Marshall D. Teach", description: "Bounty: 3,996,000,000 | Only man with 2 devil fruits." },
+    { category: "pirate", image: "images/ace.png", title: "Portgas D. Ace", description: "Bounty: 550,000,000 | Fire fist Ace." },
+    { category: "pirate", image: "images/boa.png", title: "Boa Hancock", description: "Bounty: 1,659,000,000 | Pirate Empress." }
 ];
+
