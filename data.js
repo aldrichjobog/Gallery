bcb9ed94afdf1efab97b4daa6dc6efe7e0ffbe5e
aldrichjobog <aldrichjobog18@gmail.com>
2025-11-25 12:20:17 +0800

    simple one piece gallery

---
+++ data.js
@@ -0,0 +1,101 @@
+var galleryItems = [
+    // ======== NAVY MARINES (8) ========
+    {
+        category: "marine",
+        image: "images/sengoku.jpg",
+        title: "Sengoku",
+        description: "Position: Fleet Admiral"
+    },
+    {
+        category: "marine",
+        image: "images/garp.jpg",
+        title: "Monkey D. Garp",
+        description: "Position: Vice Admiral"
+    },
+    {
+        category: "marine",
+        image: "images/akainu.jpg",
+        title: "Akainu (Sakazuki)",
+        description: "Position: Fleet Admiral"
+    },
+    {
+        category: "marine",
+        image: "images/aokiji.jpg",
+        title: "Aokiji (Kuzan)",
+        description: "Position: Admiral"
+    },
+    {
+        category: "marine",
+        image: "images/kizaru.jpg",
+        title: "Kizaru (Borsalino)",
+        description: "Position: Admiral"
+    },
+    {
+        category: "marine",
+        image: "images/fujitora.jpg",
+        title: "Fujitora (Issho)",
+        description: "Position: Admiral"
+    },
+    {
+        category: "marine",
+        image: "images/smoker.jpg",
+        title: "Smoker",
+        description: "Position: Vice Admiral"
+    },
+    {
+        category: "marine",
+        image: "images/tashigi.jpg",
+        title: "Tashigi",
+        description: "Position: Captain"
+    },
+
+    // ======== PIRATES (8) ========
+    {
+        category: "pirate",
+        image: "images/luffy.jpg",
+        title: "Monkey D. Luffy",
+        description: "Bounty: 3,000,000,000 Berries"
+    },
+    {
+        category: "pirate",
+        image: "images/zoro.jpg",
+        title: "Roronoa Zoro",
+        description: "Bounty: 1,111,000,000 Berries"
+    },
+    {
+        category: "pirate",
+        image: "images/law.jpg",
+        title: "Trafalgar D. Water Law",
+        description: "Bounty: 3,000,000,000 Berries"
+    },
+    {
+        category: "pirate",
+        image: "images/kid.jpg",
+        title: "Eustass Kid",
+        description: "Bounty: 3,000,000,000 Berries"
+    },
+    {
+        category: "pirate",
+        image: "images/shanks.jpg",
+        title: "Shanks",
+        description: "Bounty: 4,048,900,000 Berries"
+    },
+    {
+        category: "pirate",
+        image: "images/blackbeard.jpg",
+        title: "Marshall D. Teach (Blackbeard)",
+        description: "Bounty: 3,996,000,000 Berries"
+    },
+    {
+        category: "pirate",
+        image: "images/ace.jpg",
+        title: "Portgas D. Ace",
+        description: "Bounty: 550,000,000 Berries"
+    },
+    {
+        category: "pirate",
+        image: "images/boa.jpg",
+        title: "Boa Hancock",
+        description: "Bounty: 1,659,000,000 Berries"
+    }
+];
